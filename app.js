437e1f670206bfe10a7b0f80ebe0dc74d911b60d
RrChaos <rrchaos@tutanota.com>
2018-12-15 04:27:25 +0000

    interviewsQuizzAppVanillaJS

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 //-------------------------------------------------------------------------------
 //DATA MODULE
-const dataController = (function() {
 
+const dataController = (function() {
+    // class Input (questions)
+    class Input {
+        constructor(id, question, answers, rightOne) {
+            this.id = id;
+            this.question = question;
+            this.answers = answers;
+            this.rightAns = rightOne;
+        }
+    }
     return {
+        dataBase: function(question, answers){
+            
+        }
 
     }
 })();
+
 
 //-------------------------------------------------------------------------------
 //UI MODULE
-const UIController = (function() {
 
+const UIController = (function() {
+    const DOM = {
+        insert: document.getElementById('question-insert-btn')
+    }
     return {
+        getDom: DOM
 
     }
 })();
 
+
 //-------------------------------------------------------------------------------
 //EVENTS MODULE
-const EController = (function() {
 
-    return {
+const EController = (function(da, ui) {
+    // Input click Event
+    const input = ui.getDom.insert;
+    input.addEventListener('click', function() {
+        console.log('wroks');
+    });
 
-    }
 })(dataController, UIController);
